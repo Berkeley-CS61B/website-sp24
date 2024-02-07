d0ca9cb8d196288a4ca5ae068c7738aedb315f05
Erik Nelson <erik.nelson@berkeley.edu>
2024-02-06 17:31:02 -0800

    add lab04 script

---
+++ labs/lab04/lab04.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euf -o pipefail
+
+result=${PWD##*/}          # to assign to a variable
+result=${result:-/}        # to correct for the case where PWD=/
+
+if [[ $result == *"sp24-s"* ]]; then
+  printf "
+   ________________________________________
+  / You f*cked up! Make sure you run the   \\
+  \ script outside of your student folder. /
+   ----------------------------------------
+          \   ^__^
+           \  (oo)\_______
+              (__)\       )\/\\
+                  ||----w |
+                  ||     ||
+  "
+  echo
+  exit 0
+fi
+
+git clone "git@github.com:Berkeley-CS61B/git-exercise-sp24.git"
